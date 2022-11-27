dcbd0b7f6ae1cbf8bbe4c0c09bf73edefb2942ff
Stephen Muchendu <muchendumaina66@gmail.com>
2022-11-27 15:47:16 +0300

    Support

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  Header,
+  Nav,
+  About,
+  Experience,
+  Services,
+  Portfolio,
+  Footer,
+  Testimonials,
+  Contact
+} from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 }
